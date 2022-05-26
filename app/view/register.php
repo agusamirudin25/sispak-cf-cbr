4c651be26f4a2cb4e11e77a630408091226fcc18
Agus Amirudin <agusamirudin1996@gmail.com>
2022-05-26 11:15:44 +0700

    update register page

--- app/view/register.php
+++ app/view/register.php
@@ -24,23 +24,7 @@
                 <!-- content @s -->
                 <div class="nk-content ">
                     <div class="nk-split nk-split-page nk-split-md">
-                        <div class="nk-split-content nk-split-stretch bg-lighter d-flex toggle-break-lg toggle-slide toggle-slide-right" data-content="athPromo" data-toggle-screen="lg" data-toggle-overlay="true">
-                            <div class="slider-wrap w-100 w-max-550px p-3 p-sm-5 m-auto">
-                                <div class="slider-init" data-slick='{"dots":false, "arrows":false}'>
-                                    <div class="slider-item">
-                                        <div class="nk-feature nk-feature-center">
-                                            <div class="nk-feature-img">
-                                                <img class="round" src="<?= asset('assets/images/bengkel.jpg') ?>" alt="">
-                                            </div>
-                                            <div class="nk-feature-content py-4 p-sm-5">
-                                                <p>Sistem Pakar Mendiagnosa Kerusakan Pada Mesin Mobil Matic</p>
-                                            </div>
-                                        </div>
-                                    </div><!-- .slider-item -->
-                                </div><!-- .slider-init -->
-                            </div><!-- .slider-wrap -->
-                        </div><!-- .nk-split-content -->
-                        <div class="nk-split-content nk-block-area nk-block-area-column nk-auth-container bg-white">
+                        <div class="nk-split-content nk-block-area nk-block-area-column bg-white">
                             <div class="absolute-top-right d-lg-none p-3 p-sm-5">
                                 <a href="#" class="toggle btn-white btn btn-icon btn-light" data-target="athPromo"><em class="icon ni ni-info"></em></a>
                             </div>
