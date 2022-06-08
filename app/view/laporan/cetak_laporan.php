6e78f7b22310b34c7f6e6fb5352b0cf33d77d29d
Agus Amirudin <agusamirudin1996@gmail.com>
2022-06-08 19:40:57 +0700

    add logo d cetak laporan

--- app/view/laporan/cetak_laporan.php
+++ app/view/laporan/cetak_laporan.php
@@ -1,7 +1,10 @@
 <link rel="stylesheet" href="<?= asset('assets/css/dashlite.css?ver=2.2.0') ?>">
 <body>
     <div class="container" style="margin-top: 15rem;">
-        <h2 style="text-align: center;">Laporan Bulan <?= $bulan ?> Tahun <?= $tahun ?></h2>
+    <div class="text-center">
+    <img src="<?= asset('assets/images/logo-sispak.png') ?>" alt="" width="15%" class="img img-fluid">
+    </div>
+        <h2 style="text-align: center;">Laporan <?= $bulan ? 'Bulan ' . $bulan : ''  ?> <?= $tahun ? 'Tahun ' . $tahun : '' ?></h2>
         <table class="table table-bordered mt-3">
             <thead>
                 <tr>
@@ -26,6 +29,22 @@
                 </tr>
             <?php endforeach; ?>
             </tbody>
+        </table>
+
+        
+        <table style="border: 0; border-collapse: collapse; margin-top: 6em;">
+            <tr>
+                <td style="width: 80%;">&nbsp;</td>
+                <td>
+                    <p>Jakarta, <?= tgl_indo(date('d-m-Y')) ?></p>
+                    <p>Kepala Bengkel</p>
+                    <br>
+                    <br>
+                    <br>
+                    <br>
+                    <p>Siti Nurjanah </p>
+                </td>
+            </tr>
         </table>
 
     </div>
